fc31633945773f3fa51ca35fa8e91405559da55e
Justin-p <justin-p@users.noreply.github.com>
2022-06-01 14:55:14 +0200

    update

--- exercises/02_babies_first_playbook/convert_me.sh
+++ exercises/02_babies_first_playbook/convert_me.sh
@@ -1,15 +1,25 @@
 # Install nginx
 apt install nginx --quiet -y nginx
 
+# Start nginx and configure it to run on boot
+service nginx start
+systemctl enable nginx
+
 # Copy over files to webroot
 cp index.html /var/www/html/index.html
 cp index.css /var/www/html/index.css
 cp man.gif /var/www/html/man.gif
 cp aesthetic.css /var/www/html/aesthetic.css
 
-# start nginx and configure it to run on boot
-service nginx start
-systemctl enable nginx
+# Upload new vhost
+cp default.vhost /etc/nginx/sites-enabled/default.vhost
+
+# Remove existing vhost
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+
+# Retart nginx to load new vhost
+service nginx restart
 
 # Allow ssh, http, https and enable the firewall
 ufw allow 22
